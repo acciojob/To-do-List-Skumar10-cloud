d68a24f620efd76fa84398c7d6e72728a3fd0f92
Skumar10-cloud <120595419+Skumar10-cloud@users.noreply.github.com>
2023-02-28 16:40:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
+var todoValue = document.getElementById("newTodoInput");
+var btn = document.getElementById("addTodoBtn");
+var ol = document.getElementById("todoList");//to assign li to ol
 
+function addTodo() //use function because on add todo page isn't working
+ {
+	if(todoValue.value){
+	// we need to create a li
+	var li = document.createElement('li');
+	li.textContent = todoValue.value;
+	ol.appendChild(li);//relatable to 4th row of js
+	todoValue.value = "";
+	}
+}
+
+btn.addEventListener('click',addTodo); //whenever perform this click then This gona call addTodo
